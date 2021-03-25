34b47a05f8172a770904e5aebecc955e414a79e7
marniwild <marniwild49@gmail.com>
2021-03-26 11:00:59 +1300

    server/index

---
+++ routes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.router()
+const fs = require('fs')
+const file = require('./file')
+
+
+router.get('quiz/:id', (req, res) => {
+    id = Number(req.params.id)
+
+    path = "./questions.json"
+    file.getQuestion(path, id, cb)
+
+    function cb(data) {
+        res.render("Quiz", data)
+
+    }
+})
+
+
+
+// Quiz Question do you sleep alot ? 
+
+// potato 1 point
+
+
+// you are a potato
+
