3f0b4d30e28373ffbc16153888bbdeb29901372b
Louis <louis.earl.14@gmail.com>
2021-03-29 13:27:57 +1300

    adds dirname for views filepath

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const path = require('path')
 
 const routes = require('./routes')
 
 const server = express()
 
 // Server configuration
-server.use(express.static("public"))
+server.use(express.static(path.join(__dirname, 'public')));
 server.use(express.urlencoded({ extended: false }))
 
 // Handlebars configuration
 server.engine("hbs", hbs({ extname: "hbs" }))
 server.set("view engine", "hbs")
-server.set('views', './views');
+server.set('views', path.join(__dirname, 'views'));
 
 
 // Routes
